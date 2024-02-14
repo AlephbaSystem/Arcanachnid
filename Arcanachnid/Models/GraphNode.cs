36da0ab17dc7b77df068d3ec5f38900a862b1109
Soroush Trb. <tspersian@gmail.com>
2024-02-14 12:15:53 +0330

    graph model with more details

--- Arcanachnid/Models/GraphNode.cs
+++ Arcanachnid/Models/GraphNode.cs
@@ -9,11 +9,27 @@ namespace Arcanachnid.Models
     public class GraphNode
     {
         public string Title { get; set; }
+        public string Category { get; set; }
         public string Body { get; set; }
         public string Url { get; set; }
         public string PostId { get; set; }
+        public string Date { get; set; }
+        public List<(string, string)> References { get; set; }
+        public List<string> Tags { get; set; }
         public List<GraphNode> ChildNodes { get; set; }
 
+        public GraphNode(string title, string body, string category, string url, string postId, string date, List<(string, string)> references, List<string> tags)
+        {
+            Title = title;
+            Body = body;
+            Url = url;
+            Tags = tags;
+            PostId = postId;
+            Date = date;
+            references = references;
+            category = category;
+            ChildNodes = new List<GraphNode>();
+        }
         public GraphNode(string title, string body, string url, string postId)
         {
             Title = title;
@@ -27,5 +43,4 @@ namespace Arcanachnid.Models
             ChildNodes = new List<GraphNode>();
         }
     }
-
 }
